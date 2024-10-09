d77235f9a9c26adcd1a9c36f8150d469ef37f84f
mviana836 <marceloviana836@gmail.com>
2024-10-09 15:30:03 -0300

    correção de segurança

--- contact.php
+++ contact.php
@@ -6,6 +6,13 @@ $nome = $_POST['name'];
 $email = $_POST['email'];
 $mensagem = $_POST['message'];
 
+// Carrega o autoload do Composer para usar o dotenv
+require 'vendor/autoload.php';
+
+// Carrega as variáveis de ambiente do arquivo .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Carregar o PHPMailer
 require 'PHPMailer/PHPMailerAutoload.php';
 
@@ -13,13 +20,16 @@ $mail = new PHPMailer;
 $mail->isSMTP();                        // Usar SMTP
 $mail->Host = 'smtp.gmail.com';         // Servidor SMTP do Gmail
 $mail->SMTPAuth = true;                 // Habilitar autenticação SMTP
-$mail->Username = 'marceloviana836@gmail.com'; // Seu e-mail do Gmail
-$mail->Password = 'uneizglzlcrnkpht';   // Sua senha de app do Gmail
+
+// Acessa as variáveis de ambiente para o email e senha
+$mail->Username = $_ENV['EMAIL_USERNAME']; // Variável de ambiente para o email
+$mail->Password = $_ENV['EMAIL_PASSWORD']; // Variável de ambiente para a senha
+
 $mail->SMTPSecure = 'tls';              // Usar TLS (pode ser SSL para a porta 465)
 $mail->Port = 587;                      // Porta para TLS (587)
 
 // Configurações do e-mail
-$mail->setFrom('marceloviana836@gmail.com', 'Marcelo Viana'); // Remetente
+$mail->setFrom($_ENV['EMAIL_USERNAME'], 'Marcelo Viana'); // Remetente
 $mail->addAddress($email, $nome);        // Destinatário
 $mail->isHTML(true);                     // Enviar como HTML
 $mail->Subject = 'Contato Portifolio';   // Assunto
